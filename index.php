ce19303bcb7dbd1a6b733a0602494267c24fe5e4
青山 <102872581+iQingshan@users.noreply.github.com>
2024-04-01 17:10:04 +0800

    Update index.php
    建议修改增加cookie登录

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ $pwd = urlencode($_REQUEST['pwd']);
 
 if(isset($user)){
     $url = "http://搭建的接口地址/login/cellphone?phone=$user&password=$pwd";//此处为登录接口 以github上nodejsApi项目为例
-    $ip = '';#你的IP
+    $ip = ''; 	#你的IP
+    $cookie = '';	#填写cookie
     $header = array('Content-Type:'.'application/x-www-form-urlencoded;charset=UTF-8','X-FORWARDED-FOR:'.$ip,'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36','Upgrade-Insecure-Requests: 1');
     $output = curl_get($url,$header,$cookie='');
     $output = json_decode($output,1);
@@ -20,7 +21,13 @@ if(isset($user)){
         $output = curl_get($url,$header,$cookie);
         print_r($output);
     }else{
-        echo "{'code':'202','msg':'登陆失败'}";
+	if(isset($cookie) && $cookie!=''){
+		$url = "http://你的域名/Score.php?do=dask";
+        	$output = curl_get($url,$header,$cookie);
+       		print_r($output);
+	}else{
+       		echo "{'code':'202','msg':'登陆失败'}";
+	}
     }
 }else{
     echo "{'code':'203','msg':'本接口暂时免费使用，会自动关注作者网易云，介意勿用。简要使用说明：参数：user(账号)/pwd(密码) 方式：GET/POST'}";
